136fc4739d34810b61e9426f6216a481a2865d96
Puru Vaish <puruvaish24@outlook.com>
2022-02-03 13:36:31 +0100

    Change: implement heuristic-al epsilon scaling to avoid catastrophic learning, and train more adversarial models.

--- trainer.py
+++ trainer.py
@@ -1,4 +1,5 @@
 import random
+import time
 from fractions import Fraction
 
 import torch
@@ -6,12 +7,12 @@ from torchattacks import FGSM, RFGSM, MIFGSM, PGD
 
 
 def identity(woof, *args, **kwargs):
-    __name__ = 'identity'
     return woof
 
 
 def epoch_train(model, training_data_loader, data_augmentation, optimiser, criterion, training_device,
-                adv_training, ensemble_list, l_ep, u_ep):
+                adv_training, ensemble_list, u_ep, best_acc_seen):
+    epsilon_scaling = best_acc_seen
     for i, (x_input, y_target) in enumerate(training_data_loader):
 
         if adv_training:
@@ -19,21 +20,13 @@ def epoch_train(model, training_data_loader, data_augmentation, optimiser, crite
                 adv_model = random.choice(ensemble_list)
             else:
                 adv_model = model
-            # mi_steps = 5
-            # epsilon = l_ep + (u_ep - l_ep) * random.random()
+            steps = 5
+            epsilon = epsilon_scaling * u_ep * random.random()
             attack = random.choice([
-                MIFGSM(adv_model),
-                identity,
-                identity,
-                FGSM(adv_model),
-                identity,
-                identity,
-                RFGSM(adv_model),
-                identity,
-                identity,
-                PGD(adv_model),
-                identity,
-                identity,
+                MIFGSM(adv_model, epsilon, alpha=epsilon / (steps - 1), steps=steps), identity, identity,
+                FGSM(adv_model, epsilon), identity, identity,
+                RFGSM(adv_model, epsilon), identity, identity,
+                PGD(adv_model, epsilon, alpha=epsilon / (steps - 1), steps=steps), identity, identity,
             ])
         else:
             attack = None
@@ -46,6 +39,7 @@ def epoch_train(model, training_data_loader, data_augmentation, optimiser, crite
         if adv_training:
             x_adv = attack(x_input, y_target)
             y_pred = model(x_adv)
+            del x_adv
         else:
             y_pred = model(x_input)
         loss = criterion(y_pred, y_target)
@@ -53,20 +47,32 @@ def epoch_train(model, training_data_loader, data_augmentation, optimiser, crite
         loss.backward()
         optimiser.step()
 
-        if not i % 50:
+        if not i % 25:
             # Compute relevant metrics
             y_pred_max = torch.argmax(y_pred, dim=1)  # Get the labels with the highest output probability
-            correct = torch.sum(torch.eq(y_pred_max, y_target)).item()  # Count how many are equal to the true labels
+            correct = float(torch.sum(torch.eq(y_pred_max, y_target)).item())  # Count num equal to the true label
+            train_acc = correct / len(x_input)
+
+            if attack is identity:
+                attack_name = identity.__name__
+            elif attack is None:
+                attack_name = 'None'
+            else:
+                epsilon_scaling = train_acc
+                attack_name = f'{type(attack).__name__}-ep:{attack.eps}'
 
             print(f'b-{i}'.ljust(10),
-                  f'train accuracy: {correct / len(x_input)}'.ljust(30),
+                  f'train accuracy: {train_acc}'.ljust(30),
                   f'loss: {loss:.4f}'.ljust(15),
-                  f'attack: {type(attack).__name__}'.ljust(20))
+                  f'attack: {attack_name}'.ljust(30))
+            del correct, y_pred_max, attack_name
+
+        del x_input, y_target, y_pred, loss
 
 
 def train_suit(model, num_epochs, training_data_loader, data_augmentation, optimiser, criterion,
                use_cuda=True, save_model=False, path_to_file='.',
-               adv_training=False, ensemble_list=None, lower_bound_ep=0.05, upper_bound_ep=0.40,
+               adv_training=False, ensemble_list=None, upper_bound=64 / 255,
                testing_data_loader=None, testing_length=None, test_aug=None):
     if use_cuda and torch.cuda.is_available():
         device = torch.device('cuda')
@@ -89,13 +95,14 @@ def train_suit(model, num_epochs, training_data_loader, data_augmentation, optim
         best_so_far = 0
 
     print(f'Starting Training -> best_so_far = {best_so_far}')
+    start_time = time.perf_counter()
     for epoch_num in range(num_epochs):
         print(f'Starting epoch: {epoch_num}')
 
         model.train()
         epoch_train(model, training_data_loader, data_augmentation, optimiser, criterion,
                     training_device=device, adv_training=adv_training, ensemble_list=ensemble_list,
-                    l_ep=lower_bound_ep, u_ep=upper_bound_ep)
+                    u_ep=min([((epoch_num + 2)/num_epochs) * upper_bound, upper_bound]), best_acc_seen=best_so_far)
 
         if testing_length is not None and testing_data_loader is not None:
 
@@ -113,7 +120,8 @@ def train_suit(model, num_epochs, training_data_loader, data_augmentation, optim
                     y_pred = model(x_batch)
                     y_pred_max = torch.argmax(y_pred, dim=1)
 
-                    correct_total += torch.sum(torch.eq(y_pred_max, y_batch)).item()
+                    correct_total += float(torch.sum(torch.eq(y_pred_max, y_batch)).item())
+                    del x_batch, y_batch, y_pred, y_pred_max
 
                 accuracy = correct_total / testing_length
                 print(f'Accuracy on the test set: {accuracy:.3f}')
@@ -124,9 +132,14 @@ def train_suit(model, num_epochs, training_data_loader, data_augmentation, optim
                     print(f'Model saved! -> best_so_far = {best_so_far}')
                 else:
                     print(f'current / best_so_far = {accuracy} / {best_so_far}')
+                del accuracy, correct_total
         else:
             if save_model:
                 torch.save((model.state_dict(), optimiser.state_dict(), best_so_far), path_to_file)
+
+    end_time = time.perf_counter()
+    duration = end_time - start_time
+    print(f'Training took: {duration:.3f}s'.ljust(30), f'{duration / num_epochs} s/epoch')
 
 
 def robust_test_suit(model, path_to_file, testing_data_loader, testing_length, test_aug=None, attacks=None,
@@ -146,8 +159,7 @@ def robust_test_suit(model, path_to_file, testing_data_loader, testing_length, t
     try:
         (model_dict, _, clean_acc) = torch.load(path_to_file, device)
         model.load_state_dict(model_dict)
-        print('Old Model State Loaded!')
-        print(path_to_file)
+        print('Old Model State Loaded!', path_to_file)
     except (FileNotFoundError, IOError) as e:
         print('Saved model not found!')
         raise ModuleNotFoundError
@@ -155,7 +167,14 @@ def robust_test_suit(model, path_to_file, testing_data_loader, testing_length, t
     model.eval()
 
     if not attacks:
-        attacks = [MIFGSM(model), FGSM(model), PGD(model)]
+        attacks = [
+            FGSM(model, 4 / 255), FGSM(model, 8 / 255), FGSM(model, 16 / 255), FGSM(model, 32 / 255),
+            FGSM(model, 64 / 255),
+            MIFGSM(model, 4 / 255, 1 / 255, 5), MIFGSM(model, 8 / 255, 2 / 255, 5), MIFGSM(model, 16 / 255, 4 / 255, 5),
+            MIFGSM(model, 32 / 255, 8 / 255, 5), MIFGSM(model, 64 / 255, 16 / 255, 5),
+            PGD(model, 4 / 255, 1 / 255, 5), PGD(model, 8 / 255, 2 / 255, 5), PGD(model, 16 / 255, 4 / 255, 5),
+            PGD(model, 32 / 255, 8 / 255, 5), PGD(model, 64 / 255, 16 / 255, 5),
+        ]
 
     print(f'Accuracy on clean test set: {clean_acc}')
     for attack in attacks:
@@ -172,9 +191,14 @@ def robust_test_suit(model, path_to_file, testing_data_loader, testing_length, t
             y_pred = model(x_batch_adv)
             y_pred_max = torch.argmax(y_pred, dim=1)
 
-            correct_total += torch.sum(torch.eq(y_pred_max, y_batch)).item()
+            correct_total += float(torch.sum(torch.eq(y_pred_max, y_batch)).item())
+            del x_batch, y_batch, x_batch_adv, y_pred, y_pred_max
 
         accuracy = correct_total / testing_length
         print(f'{attack_name}'.ljust(10),
               f'ep:{Fraction(attack.eps).limit_denominator()}'.ljust(15),
               f'Accuracy on the test set: {accuracy:.4f}'.ljust(40))
+
+        del accuracy, correct_total, attack_name
+
+    del clean_acc, model
